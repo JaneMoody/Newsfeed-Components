2a809b176a9d24d0d15499ed633552ef6afefb8b
jane_moody <janeemoody2@gmail.com>
2020-12-02 08:47:23 -0500

    Added a new button page in js components folder.

---
+++ components/Buttons.js
@@ -0,0 +1,25 @@
+const data = ["Button 1", "Button 2", "Button 3", "Button 4"];
+
+function buttonCreator(buttonText) {
+  const button = document.createElement("button");
+  button.textContent = buttonText;
+  button.classList.add("button");
+  button.addEventListener("click", (e) => {
+    console.log("clicked!");
+  });
+  return button;
+}
+
+data.forEach((arrayItem) => {
+  let newButton = buttonCreator(arrayItem);
+  parent.appendChild(newButton);
+});
+
+let newComponents = data.map((arrayItem) => {
+  let newButton = buttonCreator(arrayItem);
+  return newButton;
+});
+
+newComponents.forEach((component) => {
+  parent.appendChild(component);
+});
